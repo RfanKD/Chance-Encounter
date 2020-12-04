f34fad6334206962df5fe868c9ef36c8977207e0
Rfan-PC\Rfan <rfan.kd@gmail.com>
2020-12-04 15:14:14 -0500

    Raphael - cron job + post phone number

---
+++ go-cron-job/cronTask.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/neo4j/neo4j-go-driver/neo4j"
+)
+
+type User struct {
+	Id           int    `json:"Id,omitempty"`
+	Name         string `json:"Name,omitempty"`
+	Email        string `json:"Email,omitempty"`
+	PhoneNumber  string `json:"PhoneNumber,omitempty"`
+	Status       string `json:"Status,omitempty"`
+	Availability string `json:"Availability,omitempty"`
+}
+
+type response struct {
+	Numbers []string `json:"numbers"`
+}
+
+//	"github.com/aws/aws-lambda-go/lambda"
+func main() {
+	//lambda.Start(timeDisplayHandler)
+
+	configForNeo4j40 := func(conf *neo4j.Config) { conf.Encrypted = false }
+
+	//user := new([]User)
+	driver, err := neo4j.NewDriver("bolt://72.140.181.254:7687", neo4j.BasicAuth("neo4j_dbuser", "password", ""), configForNeo4j40)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// handle driver lifetime based on your application lifetime requirements
+	// driver's lifetime is usually bound by the application lifetime, which usually implies one driver instance per application
+	defer driver.Close()
+
+	// For multidatabase support, set sessionConfig.DatabaseName to requested database
+	sessionConfig := neo4j.SessionConfig{AccessMode: neo4j.AccessModeRead}
+	session, err := driver.NewSession(sessionConfig)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer session.Close()
+
+	callList, err := session.ReadTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+		callList := [][]string{}
+
+		//"WHERE exists((u)-[:Available_at]->(:Time {Start_time1:'"+ time.Now() +"'}))"+
+
+		result, err := tx.Run("Match (u:User)-[r:Encounters_with]->(encounter:User)"+
+			"WHERE exists((u)-[:Available_at]->(:Time {Start_time1:'2:00pm'}))"+
+			"AND exists((encounter)-[:Available_at]->(:Time {Start_time1:'2:00pm'}))"+
+			"Return u.name, u.PhoneNumber, encounter.name, encounter.PhoneNumber, r.Available_at", nil)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		for result.Next() {
+			numlist := []string{result.Record().GetByIndex(1).(string), result.Record().GetByIndex(3).(string)}
+			callList = append(callList, numlist)
+		}
+
+		if err = result.Err(); err != nil {
+			log.Fatalln(err)
+		}
+
+		return callList, nil
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	test(callList)
+	timeDisplayHandler()
+}
+
+func test(t interface{}) {
+	switch reflect.TypeOf(t).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(t)
+
+		for i := 0; i < s.Len(); i++ {
+			//fmt.Println(s.Index(i))
+			y := s.Index(i).Interface().([]string)
+			sendNumbers(y)
+		}
+	}
+}
+
+func timeDisplayHandler() {
+	fmt.Println(strings.ToLower(time.Now().Format(time.Kitchen)))
+	// configForNeo4j35 := func(conf *neo4j.Config) {}
+}
+
+func sendNumbers(numList []string) {
+
+	res1D := &response{
+		Numbers: numList}
+	requestBody, err := json.Marshal(res1D)
+	fmt.Println(string(requestBody))
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, err := http.Post("", "application/json", bytes.NewBuffer(requestBody))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(string(body))
+
+}
---
+++ go-cron-job/go.mod
@@ -0,0 +1,17 @@
+module cron.com/cron/cron-job
+
+go 1.15
+
+require (
+	github.com/aws/aws-lambda-go v1.20.0
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/inconshreveable/log15 v0.0.0-20201112154412-8562bdadbbac // indirect
+	github.com/kevinburke/go-types v0.0.0-20200309064045-f2d4aea18a7a // indirect
+	github.com/kevinburke/go.uuid v1.2.0 // indirect
+	github.com/kevinburke/rest v0.0.0-20200429221318-0d2892b400f8 // indirect
+	github.com/kevinburke/twilio-go v0.0.0-20200810163702-320748330fac // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/neo4j/neo4j-go-driver v1.8.3
+	github.com/ttacon/builder v0.0.0-20170518171403-c099f663e1c2 // indirect
+	github.com/ttacon/libphonenumber v1.1.0 // indirect
+)
